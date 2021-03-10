f634ff1129c729d5aaedae4ddc19c24ffa1ceaa8
Oren Sokolowsky <oren@daostack.io>
2021-03-10 09:23:16 +0200

    HATMaster

---
+++ contracts/HATMaster.sol
@@ -0,0 +1,209 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.7.6;
+
+
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
+import "openzeppelin-solidity/contracts/utils/EnumerableSet.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+import "./HATToken.sol";
+
+
+contract HATMaster is Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    struct UserInfo {
+        uint256 amount;     // How many LP tokens the user has provided.
+        uint256 rewardDebt; // Reward debt. See explanation below.
+    }
+
+    // Info of each pool.
+    struct PoolInfo {
+        IERC20 lpToken;
+        uint256 allocPoint;
+        uint256 lastRewardBlock;
+        uint256 rewardPerShare;
+    }
+
+    HATToken public HAT;
+
+    uint256 public REWARD_PER_BLOCK;
+    uint256 public START_BLOCK;
+
+    // Info of each pool.
+    PoolInfo[] public poolInfo;
+    mapping(address => uint256) public poolId1; // poolId1 count from 1, subtraction 1 before using with poolInfo
+    // Info of each user that stakes LP tokens. pid => user address => info
+    mapping (uint256 => mapping (address => UserInfo)) public userInfo;
+    // Total allocation poitns. Must be the sum of all allocation points in all pools.
+    uint256 public totalAllocPoint = 0;
+
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event SendReward(address indexed user, uint256 indexed pid, uint256 amount);
+
+    constructor(
+        HATToken _HAT,
+        uint256 _rewardPerBlock,
+        uint256 _startBlock
+    ) public {
+        HAT = _HAT;
+        REWARD_PER_BLOCK = _rewardPerBlock;
+        START_BLOCK = _startBlock;
+    }
+
+    // -------- For manage pool ---------
+    function add(uint256 _allocPoint, IERC20 _lpToken, bool _withUpdate) public onlyOwner {
+        require(poolId1[address(_lpToken)] == 0, "HATMaster::add: lp is already in pool");
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 lastRewardBlock = block.number > START_BLOCK ? block.number : START_BLOCK;
+        totalAllocPoint = totalAllocPoint.add(_allocPoint);
+        poolId1[address(_lpToken)] = poolInfo.length + 1;
+        poolInfo.push(PoolInfo({
+            lpToken: _lpToken,
+            allocPoint: _allocPoint,
+            lastRewardBlock: lastRewardBlock,
+            rewardPerShare: 0
+        }));
+    }
+
+    function set(uint256 _pid, uint256 _allocPoint, bool _withUpdate) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(_allocPoint);
+        poolInfo[_pid].allocPoint = _allocPoint;
+    }
+
+    function massUpdatePools() public {
+        uint256 length = poolInfo.length;
+        for (uint256 pid = 0; pid < length; ++pid) {
+            updatePool(pid);
+        }
+    }
+
+    function updatePool(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        if (block.number <= pool.lastRewardBlock) {
+            return;
+        }
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (lpSupply == 0) {
+            pool.lastRewardBlock = block.number;
+            return;
+        }
+        uint256 reward = getPoolReward(pool.lastRewardBlock, block.number, pool.allocPoint);
+
+        HAT.mint(address(this), reward);
+        pool.rewardPerShare = pool.rewardPerShare.add(reward.mul(1e12).div(lpSupply));
+        pool.lastRewardBlock = block.number;
+    }
+
+    function deposit(uint256 _pid, uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        updatePool(_pid);
+        if (user.amount > 0) {
+            uint256 pending = user.amount.mul(pool.rewardPerShare).div(1e12).sub(user.rewardDebt);
+            if(pending > 0) {
+                safeTransferReward(msg.sender, pending);
+            }
+        }
+        if(_amount > 0) {
+            pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+            user.amount = user.amount.add(_amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.rewardPerShare).div(1e12);
+        emit Deposit(msg.sender, _pid, _amount);
+    }
+
+    function withdraw(uint256 _pid, uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        require(user.amount >= _amount, "withdraw: not good");
+        updatePool(_pid);
+        uint256 pending = user.amount.mul(pool.rewardPerShare).div(1e12).sub(user.rewardDebt);
+        if(pending > 0) {
+            safeTransferReward(msg.sender, pending);
+        }
+        if(_amount > 0) {
+            user.amount = user.amount.sub(_amount);
+            pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.rewardPerShare).div(1e12);
+        emit Withdraw(msg.sender, _pid, _amount);
+    }
+
+    function claimReward(uint256 _pid) public {
+        deposit(_pid, 0);
+    }
+
+    function safeTransferReward(address _to, uint256 _amount) internal {
+        uint256 bal = HAT.balanceOf(address(this));
+        if (_amount > bal) {
+            HAT.transfer(_to, bal);
+        } else {
+            HAT.transfer(_to, _amount);
+        }
+    }
+
+    // Withdraw without caring about rewards. EMERGENCY ONLY.
+    function emergencyWithdraw(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        pool.lpToken.safeTransfer(address(msg.sender), user.amount);
+        emit EmergencyWithdraw(msg.sender, _pid, user.amount);
+        user.amount = 0;
+        user.rewardDebt = 0;
+    }
+
+    function getMultiplier(uint256 _from, uint256 _to) public pure returns (uint256) {
+        return _to.sub(_from);
+    }
+
+    function getPoolReward(uint256 _from, uint256 _to, uint256 _allocPoint) public view returns (uint) {
+        uint256 multiplier = getMultiplier(_from, _to);
+        uint256 amount = multiplier.mul(REWARD_PER_BLOCK).mul(_allocPoint).div(totalAllocPoint);
+        uint256 amountCanMint = HAT.seedPoolAmount();
+        return amountCanMint < amount ? amountCanMint : amount;
+    }
+
+    function getNewRewardPerBlock(uint256 pid1) public view returns (uint256) {
+        uint256 multiplier = getMultiplier(block.number -1, block.number);
+        if (pid1 == 0) {
+            return multiplier.mul(REWARD_PER_BLOCK);
+        }
+        else {
+            return multiplier
+                .mul(REWARD_PER_BLOCK)
+                .mul(poolInfo[pid1 - 1].allocPoint)
+                .div(totalAllocPoint);
+        }
+    }
+
+    function pendingReward(uint256 _pid, address _user) external view returns (uint256) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 rewardPerShare = pool.rewardPerShare;
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (block.number > pool.lastRewardBlock && lpSupply > 0) {
+            uint256 reward = getPoolReward(pool.lastRewardBlock, block.number, pool.allocPoint);
+            rewardPerShare = rewardPerShare.add(reward.mul(1e12).div(lpSupply));
+        }
+        return user.amount.mul(rewardPerShare).div(1e12).sub(user.rewardDebt);
+    }
+
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+
+    function getStakedAmount(uint _pid, address _user) public view returns (uint256) {
+        UserInfo storage user = userInfo[_pid][_user];
+        return user.amount;
+    }
+}
