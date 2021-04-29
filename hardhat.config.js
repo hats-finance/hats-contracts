a6f8cf8970e782cb2444f2984cf555f92b6cd733
Oren Sokolowsky <oren@daostack.io>
2021-04-29 21:38:03 +0300

    lint

--- hardhat.config.js
+++ hardhat.config.js
@@ -45,12 +45,6 @@ module.exports = {
      gasPrice:40000000000,
      gas: 2510000
   },
-  mainnet: {
-     url: `https://mainnet.infura.io/v3/${PRIVATE["INFURA_KEY"]}`,
-     accounts: [PRIVATE["PRIVATE_KEY"]],
-     gasPrice:40000000000,
-     gas: 2510000
-  },
   hardhat: {
   accounts: [
       {
