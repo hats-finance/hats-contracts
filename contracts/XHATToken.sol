8ffd5d635a7943ef0e0c1f3bf58ad4bf8439c69c
Oren Sokolowsky <oren@daostack.io>
2021-07-08 00:29:43 +0300

    xhattoken

---
+++ contracts/XHATToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+// This contract handles swapping to and from xHAT
+contract XHATToken is ERC20("XHATToken", "xHAT") {
+
+    IERC20 public immutable HAT;
+
+    // Define the xHAT token contract
+    constructor(IERC20 _hat) {
+        HAT = _hat;
+    }
+
+    // Locks HAT and mints xHAT
+    function swapHAT2xHAT(uint256 _amount) public {
+        _mint(msg.sender, _amount);
+        // Lock HAT in the contract
+        HAT.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    // Unlocks HAT and burn xHAT
+    function swapxHAT2HAT(uint256 _amount) public {
+        _burn(msg.sender, _amount);
+        HAT.transfer(msg.sender, _amount);
+    }
+}
