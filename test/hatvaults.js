25da876214a7546bea1a5393ab5c7eb6e78e4e7e
Oren Sokolowsky <oren@daostack.io>
2021-07-26 11:30:23 +0300

    MINIMUM_DEPOSIT

--- test/hatvaults.js
+++ test/hatvaults.js
@@ -1744,7 +1744,7 @@ contract('HatVaults',  accounts =>  {
     }
     await utils.mineBlock();
     var tx = await hatVaults.massUpdatePools(0,18);
-    assert.equal(tx.receipt.gasUsed, 1517872);
+    assert.equal(tx.receipt.gasUsed, 1517939);
   }).timeout(40000);
 
 
