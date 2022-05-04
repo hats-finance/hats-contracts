31e71c5645b04f70d46604395908201881946787
benk10 <ben.kaufman10@gmail.com>
2022-05-04 15:03:30 -0500

    fix

--- contracts/tokenlock/TokenLock.sol
+++ contracts/tokenlock/TokenLock.sol
@@ -2,8 +2,8 @@
 
 pragma solidity 0.8.6;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./OwnableInitializable.sol";
 import "./MathUtils.sol";
 import "./ITokenLock.sol";
