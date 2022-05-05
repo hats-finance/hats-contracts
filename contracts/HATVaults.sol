4d9b3595910aceef7b21eec09d2460c6980d5fb1
Liron Achdut <liron.achdut@gmail.com>
2022-05-05 15:50:05 +0300

    Update HATVaults.sol

--- contracts/HATVaults.sol
+++ contracts/HATVaults.sol
@@ -4,10 +4,10 @@
 pragma solidity 0.8.6;
 
 
-import "openzeppelin-solidity/contracts/security/ReentrancyGuard.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./Governable.sol";
 import "./HATToken.sol";
 import "./tokenlock/ITokenLockFactory.sol";
