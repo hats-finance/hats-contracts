12b7b8f27e2e04002eecf3f204a8f065bd61b247
benk10 <ben.kaufman10@gmail.com>
2022-05-17 12:58:35 -0500

    Cleanup script

--- scripts/hatvaultsdeploy.js
+++ scripts/hatvaultsdeploy.js
@@ -64,10 +64,8 @@ async function main(
       
       const facetInstance = await factory.deploy(...constructorArgs);
       await facetInstance.deployed();
-      const tx = facetInstance.deployTransaction;
-      const receipt = await tx.wait();
       if (!silent) {
-        console.log(`${facet} deploy gas used:` + strDisplay(receipt.gasUsed));
+        console.log(`${facet} deployed at: ${facetInstance.address}`);
       }
       instances.push(facetInstance);
     }
@@ -156,22 +154,6 @@ async function main(
   }
 
   return hatDiamond;
-}
-
-function addCommas (nStr) {
-  nStr += '';
-  const x = nStr.split('.');
-  let x1 = x[0];
-  const x2 = x.length > 1 ? '.' + x[1] : '';
-  var rgx = /(\d+)(\d{3})/;
-  while (rgx.test(x1)) {
-    x1 = x1.replace(rgx, '$1' + ',' + '$2');
-  }
-  return x1 + x2;
-}
-
-function strDisplay (str) {
-  return addCommas(str.toString());
 }
 
 if (require.main === module) {
