e1da16439d23ec6e22fee0bf8ba9d5b36bc43dac
jelle <jellegerbrandy@gmail.com>
2022-06-06 13:20:11 +0200

    fix typo

--- contracts/vaults/Base.sol
+++ contracts/vaults/Base.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-import "../tokenlock/tokenLockFactory.sol";
+import "../tokenlock/TokenLockFactory.sol";
 import "../RewardController.sol";
 
 contract Base is OwnableUpgradeable, ReentrancyGuardUpgradeable {
