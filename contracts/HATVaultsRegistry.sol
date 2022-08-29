d9a321a9e69106ad30945948f2122ba8ce1e0eaa
Liron Achdut <liron.achdut@gmail.com>
2022-08-29 21:48:06 +0300

    emit event

--- contracts/HATVaultsRegistry.sol
+++ contracts/HATVaultsRegistry.sol
@@ -295,6 +295,8 @@ contract HATVaultsRegistry is IHATVaultsRegistry, Ownable {
         if (swapData.amount == 0) revert AmountToSwapIsZero();
         IERC20 _HAT = HAT;
         (swapData.hatsReceived, swapData.amountUnused) = _swapTokenForHAT(IERC20(_asset), swapData.amount, _amountOutMinimum, _routingContract, _routingPayload);
+        
+        uint256 governanceAmountSwapped = (swapData.amount - swapData.amountUnused) * governanceHatReward[_asset] / swapData.amount;
         governanceHatReward[_asset] = swapData.amountUnused * governanceHatReward[_asset] / swapData.amount;
 
         for (uint256 i = 0; i < _beneficiaries.length; i++) {
@@ -325,6 +327,8 @@ contract HATVaultsRegistry is IHATVaultsRegistry, Ownable {
             emit SwapAndSend(_beneficiaries[i], hackerAmountSwapped, hackerReward, tokenLock);
         }
         _HAT.safeTransfer(owner(), swapData.hatsReceived - swapData.totalHackerReward);
+        emit SwapAndSend(owner(), governanceAmountSwapped, swapData.hatsReceived - swapData.totalHackerReward, address(0));
+
     }
 
     /** @notice See {IHATVaultsRegistry-getGeneralParameters}. */   
