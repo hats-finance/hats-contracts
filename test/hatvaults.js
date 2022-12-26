87025fc516254f9b7efea0d0c5834c91372bfc60
benk10 <ben.kaufman10@gmail.com>
2022-12-26 09:51:14 -0500

    Fix tests

--- test/hatvaults.js
+++ test/hatvaults.js
@@ -2833,12 +2833,12 @@ it("getVaultReward - no vault updates will return 0 ", async () => {
       arbitrator: vaultsManager.address,
       hatToken: hatToken1.address,
       tokenLockFactory: tokenLockFactory1.address,
-      rewardControllers: [{
+      rewardControllersConf: [{
         startBlock: 1,
         epochLength: 10,
         epochRewardPerBlock
       }],
-      hatVaultsRegistry: {
+      hatVaultsRegistryConf: {
         bountyGovernanceHAT: 1000,
         bountyHackerHATVested: 500
       },
@@ -6146,12 +6146,12 @@ it("getVaultReward - no vault updates will return 0 ", async () => {
       arbitrator: vaultsManager.address,
       hatToken: hatToken1.address,
       tokenLockFactory: tokenLockFactory1.address,
-      rewardControllers: [{
+      rewardControllersConf: [{
         startBlock: 1,
         epochLength: 10,
         epochRewardPerBlock
       }],
-      hatVaultsRegistry: {
+      hatVaultsRegistryConf: {
         bountyGovernanceHAT: 1000,
         bountyHackerHATVested: 500
       },
@@ -6197,12 +6197,12 @@ it("getVaultReward - no vault updates will return 0 ", async () => {
       arbitrator: vaultsManager.address,
       hatToken: hatToken1.address,
       tokenLockFactory: tokenLockFactory1.address,
-      rewardControllers: [{
+      rewardControllersConf: [{
         startBlock: 1,
         epochLength: 10,
         epochRewardPerBlock
       }],
-      hatVaultsRegistry: {
+      hatVaultsRegistryConf: {
         bountyGovernanceHAT: 1000,
         bountyHackerHATVested: 500
       },
