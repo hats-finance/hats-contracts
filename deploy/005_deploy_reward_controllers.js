5837abbe46959fb3b63bbc91fa12f3fac8807f9d
benk10 <ben.kaufman10@gmail.com>
2023-01-05 09:42:47 -0500

    Update 005_deploy_reward_controllers.js

--- deploy/005_deploy_reward_controllers.js
+++ deploy/005_deploy_reward_controllers.js
@@ -4,12 +4,14 @@ const { network } = require("hardhat");
 const func = async function (hre) {
     const config = CONFIG[network.name];
     const { deployments, getNamedAccounts } = hre;
-    const { deploy, execute } = deployments;
+    const { deploy } = deployments;
 
     const { deployer } = await getNamedAccounts();
 
     const rewardControllers = [];
     const rewardControllerImplementations = [];
+    let i = 1;
+
     for (const rewardControllerConfig of config["rewardControllersConf"]) {
         let startBlock = rewardControllerConfig["startBlock"];
         if (!startBlock) {
@@ -18,37 +20,32 @@ const func = async function (hre) {
         let epochLength = rewardControllerConfig["epochLength"];
         let epochRewardPerBlock = rewardControllerConfig["epochRewardPerBlock"];
 
-        // console.log(await deploy('RewardController', {
-        //     from: deployer,
-        //     args: [
-        //         (await deployments.get('HATToken')).address,
-        //         (await deployments.get('HATTimelockController')).address, 
-        //         startBlock, 
-        //         epochLength, 
-        //         epochRewardPerBlock
-        //     ],
-        //     log: true,
-        //     proxy: "OptimizedTransparentProxy"
-        // }));
-
-        // const rewardController = await upgrades.deployProxy(RewardController, [
-        //     hatToken,
-        //     governance, 
-        //     startBlock, 
-        //     epochLength, 
-        //     epochRewardPerBlock
-        // ]);
-
-        // await rewardController.deployed();
-
-        // const rewardControllerImplementation = RewardController.attach(
-        //   await getImplementationAddress(ethers.provider, rewardController.address)
-        // );
-
-        // rewardControllers.push(rewardController);
-        // rewardControllerImplementations.push(rewardControllerImplementation);
-
+        // TODO: This also deploys new implementation every time, need to find out how to only deploy a new proxy but not new implementation
+        await deploy('RewardController_' + i, {
+            contract: "RewardController",
+            from: deployer,
+            log: true,
+            proxy: {
+                proxyContract: "OpenZeppelinTransparentProxy",
+                execute: {
+                    init: {
+                        methodName: "initialize",
+                        args: [
+                            (await deployments.get('HATToken')).address, // TODO: Allow to set token in the config
+                            (await deployments.get('HATTimelockController')).address, 
+                            startBlock, 
+                            epochLength, 
+                            epochRewardPerBlock
+                        ],
+                    }
+                }
+            }
+        });
+        rewardControllers.push((await deployments.get('RewardController_' + i)).address);
+        rewardControllerImplementations.push((await deployments.get('RewardController_' + i)).implementation);
+        i++;
     }
+
 };
 module.exports = func;
 func.tags = ['RewardControllers'];
