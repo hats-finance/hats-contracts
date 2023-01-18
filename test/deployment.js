ee974d8f0a7f31ef966f5506df8039f799d67bf8
jelle <jellegerbrandy@gmail.com>
2023-01-18 18:18:49 +0100

    fix typo

--- test/deployment.js
+++ test/deployment.js
@@ -2,7 +2,7 @@
 const {deployments} = require('hardhat');
 
 describe("Deployment", function() {
-  it.only("Deployment and verification should work", async function() {
+  it("Deployment and verification should work", async function() {
     await deployments.fixture();
   });
 });
